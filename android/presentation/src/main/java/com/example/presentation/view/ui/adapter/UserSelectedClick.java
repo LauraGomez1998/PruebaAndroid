396dde7c1646689eb6913ed83ca6c2464bce477e
Laura Gómez <lagomez@heinsohn.com.co>
2020-09-14 00:36:08 -0500

    Delete presentation layer, merged into app package.

--- android/presentation/src/main/java/com/example/presentation/view/ui/adapter/UserSelectedClick.java
+++
@@ -1,8 +0,0 @@
-package com.example.presentation.view.ui.adapter;
-
-import co.com.ceiba.mobile.pruebadeingreso.model.User;
-
-public interface UserSelectedClick {
-
-    void userSelectedOnClickListener(User user);
-}
